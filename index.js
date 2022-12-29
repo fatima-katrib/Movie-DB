2aab920c70e87a29d6dd05ec2a6f44cae2d492ff
fatima-katrib <fatimakatrib19@gnail.com>
2022-12-29 19:38:12 +0200

    step 3

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 3000;
 
 app.listen(port);
-app.get("/", (req, res) => {
-  res.send("ok");
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+app.get("/time", (req, res) => {
+  let time = new Date();
+  res.send({ status: 200, message: `${time.getHours}: ${time.getMinutes}` });
 });
